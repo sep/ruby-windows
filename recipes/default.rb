061fe49e55c1954dfb7643ec17d03a0a745b276f
rmnowadly <rmnowadly@sep.com>
2017-03-06 11:00:38 -0500

    Updated issues_url and source_url in meta data. Refactored recipe to let the installer modify the path

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,16 +5,10 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 INSTALL_DIR = node["ruby-windows"]["installDir"]
-
+# Foodcritic FC009 is a false positive see https://github.com/acrmp/foodcritic/issues/451
 package "Ruby" do 
   installer_type :inno
-  source "#{node["ruby-windows"]["source"]}"
-  checksum "#{node["ruby-windows"]["SHA256"]}"
-  options "/DIR=\"#{INSTALL_DIR}\""
-end
-
-env 'Path' do
-  value "#{ INSTALL_DIR }\\bin"
-  action :modify
-  delim ';'
+  source node["ruby-windows"]["source"]
+  checksum node["ruby-windows"]["SHA256"]
+  options "/DIR=\"#{INSTALL_DIR}\" /tasks=\"assocfiles,modpath\""
 end
